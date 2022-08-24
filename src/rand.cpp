24fc4ffa7bd938e9d52f2ff161f5e71d85b7f39c
Chris Thrasher <chrisjthrasher@gmail.com>
2022-08-24 16:18:26 -0600

    Declare local variables with `auto`

--- src/rand.cpp
+++ src/rand.cpp
@@ -8,17 +8,17 @@
 namespace rsl {
 
 auto rng(std::seed_seq seed_sequence) -> std::mt19937& {
-  thread_local bool first = true;
-  thread_local std::mt19937 generator = [&seed_sequence]() {
+  thread_local auto first = true;
+  thread_local auto generator = [&seed_sequence]() {
     first = false;
     if (seed_sequence.size() > 0) {
       return std::mt19937(seed_sequence);
     }
-    std::array<int, std::mt19937::state_size> seed_data;
-    std::random_device random_device;
+    auto seed_data = std::array<int, std::mt19937::state_size>();
+    auto random_device = std::random_device();
     std::generate_n(std::data(seed_data), std::size(seed_data),
                     std::ref(random_device));
-    std::seed_seq sequence(std::begin(seed_data), std::end(seed_data));
+    auto sequence = std::seed_seq(std::begin(seed_data), std::end(seed_data));
     return std::mt19937(sequence);
   }();
   if (!first && seed_sequence.size() > 0) {
