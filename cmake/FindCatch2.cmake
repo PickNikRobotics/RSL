76e114cc342c00ef022551bde472fe26e5013172
Chris Thrasher <chrisjthrasher@gmail.com>
2022-09-23 17:17:48 -0600

    Mark Catch2 include paths as SYSTEM include paths

--- cmake/FindCatch2.cmake
+++ cmake/FindCatch2.cmake
@@ -7,4 +7,6 @@ FetchContent_Declare(Catch2
     GIT_TAG v${Catch2_FIND_VERSION})
 FetchContent_MakeAvailable(Catch2)
 set_target_properties(Catch2 PROPERTIES COMPILE_OPTIONS "")
+get_target_property(CATCH2_INCLUDE_DIRS Catch2 INTERFACE_INCLUDE_DIRECTORIES)
+target_include_directories(Catch2 SYSTEM INTERFACE ${CATCH2_INCLUDE_DIRS})
 include(Catch)
