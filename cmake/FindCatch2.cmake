9cc4bc3dcb4ec614f70fa32735beea1666eb1cee
Chris Thrasher <chrisjthrasher@gmail.com>
2022-10-04 15:47:06 -0600

    Fix CMake configuration warnings

--- cmake/FindCatch2.cmake
+++ cmake/FindCatch2.cmake
@@ -1,7 +1,5 @@
 include(FetchContent)
 
-set(CATCH_INSTALL_DOCS OFF)
-set(CATCH_INSTALL_EXTRAS OFF)
 FetchContent_Declare(Catch2
     GIT_REPOSITORY https://github.com/catchorg/Catch2.git
     GIT_TAG v${Catch2_FIND_VERSION})
