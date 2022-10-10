2fa8ec41d60123b40141778588bfedbfb6673cd2
Chris Thrasher <chrisjthrasher@gmail.com>
2022-10-10 15:23:54 -0600

    Add Doxygen comments for `TRY`

--- include/rsl/try.hpp
+++ include/rsl/try.hpp
@@ -2,21 +2,29 @@
 
 #include <tl_expected/expected.hpp>
 
-// This macro requires the use of non-standard C++. Compiler extensions must be enabled and
-// -Wpedantic cannot be used. With GCC and Clang, you can disable -Wpedantic for an entire
-// translation unit like this:
-//
-// #pragma GCC diagnostic ignored "-Wpedantic"
-//
-// You can disable -Wpedantic for a specific segment of code like so:
-//
-// #pragma GCC diagnostic push
-// #pragma GCC diagnostic ignored "-Wpedantic"
-// ...
-// #pragma GCC diagnostic pop
-//
-// For more information on this particular compiler extension go to
-// https://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html
+/**
+ * @brief Unwrap an expected into a stack variable or return the unexpected value
+ *
+ * This macro requires the use of non-standard C++. Compiler extensions must be enabled and
+ * -Wpedantic cannot be used. With GCC and Clang, you can disable -Wpedantic for an entire
+ * translation unit like this:
+ *
+ * ```cpp
+ * #pragma GCC diagnostic ignored "-Wpedantic"
+ * ```
+
+ * You can disable -Wpedantic for a specific segment of code like so:
+ *
+ * ```cpp
+ * #pragma GCC diagnostic push
+ * #pragma GCC diagnostic ignored "-Wpedantic"
+ * //...
+ * #pragma GCC diagnostic pop
+ * ```
+ *
+ * For more information on this particular compiler extension go to
+ * https://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html
+ */
 #define TRY(expected)                                                              \
     ({                                                                             \
         auto const& _expected = (expected); /* Uglify name to prevent shadowing */ \
