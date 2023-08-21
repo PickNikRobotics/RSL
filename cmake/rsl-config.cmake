63124eb98b731fde01cc7dd31692d356a65ea5bc
Chris Thrasher <chrisjthrasher@gmail.com>
2023-08-21 10:43:45 -0600

    Remove ament macros

---
+++ cmake/rsl-config.cmake
@@ -0,0 +1,7 @@
+find_dependency(Eigen3)
+find_dependency(fmt)
+find_dependency(rclcpp)
+find_dependency(tcb_span)
+find_dependency(tl_expected)
+
+include(${CMAKE_CURRENT_LIST_DIR}/rsl-targets.cmake)
