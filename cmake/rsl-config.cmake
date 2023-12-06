f103fb5235407b2c17a5e56dc0f15204dd8e7099
Chris Thrasher <chrisjthrasher@gmail.com>
2023-12-06 14:11:27 -0700

    Revert "Remove ament macros"
    This reverts commit 7cb590ab83df29880b9b39a63281fc72757d569f.

--- cmake/rsl-config.cmake
+++
@@ -1,9 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-find_dependency(Eigen3)
-find_dependency(fmt)
-find_dependency(rclcpp)
-find_dependency(tcb_span)
-find_dependency(tl_expected)
-
-include(${CMAKE_CURRENT_LIST_DIR}/rsl-targets.cmake)
