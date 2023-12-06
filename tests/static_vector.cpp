44e151e05874e1e506356fea4fc0fd5579d43507
Chris Thrasher <chrisjthrasher@gmail.com>
2023-12-06 16:08:33 -0700

    Test `rsl::StaticVector` for `int`s and `float`s

--- tests/static_vector.cpp
+++ tests/static_vector.cpp
@@ -1,47 +1,53 @@
 #include <rsl/static_vector.hpp>
 
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 
-TEST_CASE("rsl::StaticVector") {
+TEMPLATE_TEST_CASE("rsl::StaticVector", "", int, float) {
     SECTION("Type traits") {
-        STATIC_CHECK(std::is_copy_constructible_v<rsl::StaticVector<int, 8>>);
-        STATIC_CHECK(std::is_copy_assignable_v<rsl::StaticVector<int, 8>>);
-        STATIC_CHECK(std::is_nothrow_move_constructible_v<rsl::StaticVector<int, 8>>);
-        STATIC_CHECK(std::is_nothrow_move_assignable_v<rsl::StaticVector<int, 8>>);
+        STATIC_CHECK(std::is_copy_constructible_v<rsl::StaticVector<TestType, 8>>);
+        STATIC_CHECK(std::is_copy_assignable_v<rsl::StaticVector<TestType, 8>>);
+        STATIC_CHECK(std::is_nothrow_move_constructible_v<rsl::StaticVector<TestType, 8>>);
+        STATIC_CHECK(std::is_nothrow_move_assignable_v<rsl::StaticVector<TestType, 8>>);
     }
 
     SECTION("Construction") {
         SECTION("Default constructor") {
-            auto const static_vector = rsl::StaticVector<int, 10>();
+            auto const static_vector = rsl::StaticVector<TestType, 10>();
             CHECK(static_vector.begin() == static_vector.end());
         }
 
         SECTION("Collection constructor") {
-            auto const vector = std::vector<int>{1, 2, 3, 4, 5};
-            auto const static_vector = rsl::StaticVector<int, 5>(vector);
+            auto const vector = std::vector<TestType>{1, 2, 3, 4, 5};
+            auto const static_vector = rsl::StaticVector<TestType, 5>(vector);
             CHECK(static_vector.begin() != static_vector.end());
-            auto i = 1;
-            for (auto const& number : static_vector) CHECK(number == i++);
+            TestType i = 1;
+            for (auto const& number : static_vector) {
+                CHECK(number == i);
+                i += 1;
+            }
         }
 
         SECTION("Initializer list constructor") {
-            auto const static_vector = rsl::StaticVector<int, 5>{5, 4, 3, 2, 1};
+            auto const static_vector = rsl::StaticVector<TestType, 5>{5, 4, 3, 2, 1};
             CHECK(static_vector.begin() != static_vector.end());
-            auto i = 5;
-            for (auto const& number : static_vector) CHECK(number == i--);
+            TestType i = 5;
+            for (auto const& number : static_vector) {
+                CHECK(number == i);
+                i -= 1;
+            }
         }
     }
 
     SECTION("begin()") {
         SECTION("Non-const") {
-            auto static_vector = rsl::StaticVector<int, 5>{5, 4, 3, 2, 1};
+            auto static_vector = rsl::StaticVector<TestType, 5>{5, 4, 3, 2, 1};
             CHECK(static_vector.begin() == std::begin(static_vector));
             CHECK(static_vector.begin() == std::cbegin(static_vector));
             CHECK(++*static_vector.begin() == 6);
         }
 
         SECTION("Const") {
-            auto const static_vector = rsl::StaticVector<int, 5>{5, 4, 3, 2, 1};
+            auto const static_vector = rsl::StaticVector<TestType, 5>{5, 4, 3, 2, 1};
             CHECK(static_vector.begin() == std::begin(static_vector));
             CHECK(static_vector.begin() == std::cbegin(static_vector));
             CHECK(*static_vector.begin() == 5);
@@ -50,14 +56,14 @@ TEST_CASE("rsl::StaticVector") {
 
     SECTION("end()") {
         SECTION("Non-const") {
-            auto static_vector = rsl::StaticVector<int, 5>{10, 9, 8, 7, 6};
+            auto static_vector = rsl::StaticVector<TestType, 5>{10, 9, 8, 7, 6};
             CHECK(static_vector.end() == std::end(static_vector));
             CHECK(static_vector.end() == std::cend(static_vector));
             CHECK(--*(static_vector.end() - 1) == 5);
         }
 
         SECTION("Const") {
-            auto const static_vector = rsl::StaticVector<int, 5>{5, 4, 3, 2, 1};
+            auto const static_vector = rsl::StaticVector<TestType, 5>{5, 4, 3, 2, 1};
             CHECK(static_vector.end() == std::end(static_vector));
             CHECK(static_vector.end() == std::cend(static_vector));
             CHECK(*(static_vector.end() - 1) == 1);
@@ -66,8 +72,8 @@ TEST_CASE("rsl::StaticVector") {
 
     SECTION("User defined conversions") {
         SECTION("tcb::span<T>()") {
-            auto static_vector = rsl::StaticVector<int, 5>{11, 12, 13, 14, 15};
-            auto const span = tcb::span<int>(static_vector);
+            auto static_vector = rsl::StaticVector<TestType, 5>{11, 12, 13, 14, 15};
+            auto const span = tcb::span<TestType>(static_vector);
             CHECK(span[0] == 11);
             CHECK(span[1] == 12);
             CHECK(span[2] == 13);
@@ -76,8 +82,8 @@ TEST_CASE("rsl::StaticVector") {
         }
 
         SECTION("tcb::span<T const>()") {
-            auto const static_vector = rsl::StaticVector<int, 5>{20, 19, 18, 17, 16};
-            auto const span = tcb::span<int const>(static_vector);
+            auto const static_vector = rsl::StaticVector<TestType, 5>{20, 19, 18, 17, 16};
+            auto const span = tcb::span<TestType const>(static_vector);
             CHECK(span[0] == 20);
             CHECK(span[1] == 19);
             CHECK(span[2] == 18);
@@ -89,6 +95,5 @@ TEST_CASE("rsl::StaticVector") {
 
 TEST_CASE("rsl::to_vector") {
     CHECK(rsl::to_vector(rsl::StaticVector<int, 0>{}).empty());
-    CHECK(rsl::to_vector(rsl::StaticVector<int, 5>{1, 2, 3, 4, 5}) ==
-          std::vector<int>{1, 2, 3, 4, 5});
+    CHECK(rsl::to_vector(rsl::StaticVector<int, 5>{1, 2, 3, 4, 5}) == std::vector{1, 2, 3, 4, 5});
 }
