793ed0fac008ac19c23f2fb50bf4fabb0b81f4a7
Chris Thrasher <chrisjthrasher@gmail.com>
2023-12-11 16:59:58 -0700

    Stop using non-standard `M_PI`

--- src/random.cpp
+++ src/random.cpp
@@ -30,12 +30,14 @@ auto rng(std::seed_seq seed_sequence) -> std::mt19937& {
 }
 
 auto random_unit_quaternion() -> Eigen::Quaterniond {
+    static constexpr auto pi = 3.1415926535897932385;
+
     // From "Uniform Random Rotations", Ken Shoemake, Graphics Gems III, pg. 124-132
     auto const x0 = uniform_real(0., 1.);
     auto const r1 = std::sqrt(1 - x0);
     auto const r2 = std::sqrt(x0);
-    auto const t1 = uniform_real(0., 2 * M_PI);
-    auto const t2 = uniform_real(0., 2 * M_PI);
+    auto const t1 = uniform_real(0., 2 * pi);
+    auto const t2 = uniform_real(0., 2 * pi);
     auto const x = r1 * std::sin(t1);
     auto const y = r1 * std::cos(t1);
     auto const z = r2 * std::sin(t2);
