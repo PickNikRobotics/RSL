784afa9c221fc45f1e8420149db4ec6f5b0acb61
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2026-06-15 21:52:16 +0000

    Use std::numbers if available

--- src/random.cpp
+++ src/random.cpp
@@ -3,10 +3,25 @@
 #include <algorithm>
 #include <array>
 #include <functional>
+#if __has_include(<numbers>)
+#include <numbers>
+#endif
 #include <optional>
 #include <stdexcept>
 
 namespace rsl {
+
+namespace {
+
+constexpr auto kPiFallback = 3.141592653589793238462643383279502884;
+
+#if defined(__cpp_lib_math_constants) && (__cpp_lib_math_constants >= 201907L)
+constexpr auto kPi = std::numbers::pi_v<double>;
+#else
+constexpr auto kPi = kPiFallback;
+#endif
+
+}  // namespace
 
 auto rng(std::seed_seq seed_sequence) -> std::mt19937& {
     thread_local auto generator = std::optional<std::mt19937>();
@@ -30,14 +45,12 @@ auto rng(std::seed_seq seed_sequence) -> std::mt19937& {
 }
 
 auto random_unit_quaternion() -> Eigen::Quaterniond {
-    static constexpr auto pi = 3.1415926535897932385;
-
     // From "Uniform Random Rotations", Ken Shoemake, Graphics Gems III, pg. 124-132
     auto const x0 = uniform_real(0., 1.);
     auto const r1 = std::sqrt(1 - x0);
     auto const r2 = std::sqrt(x0);
-    auto const t1 = uniform_real(0., 2 * pi);
-    auto const t2 = uniform_real(0., 2 * pi);
+    auto const t1 = uniform_real(0., 2 * kPi);
+    auto const t2 = uniform_real(0., 2 * kPi);
     auto const x = r1 * std::sin(t1);
     auto const y = r1 * std::cos(t1);
     auto const z = r2 * std::sin(t2);
